0e19f2b66e37291565ea29e3e5c563195b9770fa
basat <itzbasatmaqsood@gmail.com>
2025-02-02 10:52:42 +0500

    trying to deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+module.exports = {
+    redirects: async () => [
+        {
+            source: "/game",
+            destination: "/",
+            permanent: true, // 308 Redirect
+        },
+    ],
+};
 
 export default nextConfig;
--- src/app/(main)/game/page.jsx
+++ src/app/(main)/game/page.jsx
@@ -1,15 +1,15 @@
-"use client";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+// "use client";
+// import { useRouter } from "next/navigation";
+// import { useEffect } from "react";
 
-export default function Page() {
-    const router = useRouter();
+// export default function Page() {
+//     const router = useRouter();
 
-    useEffect(() => {
-        if (router) {
-            router.replace("/");
-        }
-    }, [router]);
+//     useEffect(() => {
+//         if (router) {
+//             router.replace("/");
+//         }
+//     }, [router]);
 
-    return null; // No need to render anything
-}
+//     return null; // No need to render anything
+// }

525c2d21b9338049b7f6534237c7e8c4101a8428
basat <itzbasatmaqsood@gmail.com>
2025-02-02 11:08:32 +0500

    trying to deploy

--- src/app/(main)/game/[slug]/page.jsx
+++ src/app/(main)/game/[slug]/page.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+'use client'
+import { useParams } from 'next/navigation';
+
+import React, { Suspense } from 'react'
 
 export default function page() {
+  const { slug } = useParams(); // Get the slug from the URL
   return (
-    <Suspense>
-      
-    <div>page</div>
+    <Suspense fallback={<div>Loading...</div>}>
+    <div>{slug}</div>
     </Suspense>
   )
 }

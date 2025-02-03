7d2ad3e6956b759898314be48e5feb649ca65bff
basat <itzbasatmaqsood@gmail.com>
2025-02-03 21:59:34 +0500

    New pages added

--- src/app/(main)/sitemap.js
+++ src/app/(main)/sitemap.js
@@ -5,7 +5,7 @@ export default async function sitemap() {
   const url =
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000"
-      : process.env.NEXT_PUBLIC_WEBSITE_URL;
+      : 'https://nextrivals-basat.vercel.app/';
 
   const items = games.map((game) => ({
     url: `${url}/games/${game.slug}`,

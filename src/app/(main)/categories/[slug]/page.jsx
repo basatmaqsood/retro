09efa0910516d65a656d566a6216231929213853
basat <itzbasatmaqsood@gmail.com>
2025-02-03 19:51:58 +0500

    New pages added

--- src/app/(main)/categories/[slug]/page.jsx
+++ src/app/(main)/categories/[slug]/page.jsx
@@ -1,10 +1,12 @@
 
 import ShowGames from "@//components/ShowGames";
 import { getGamesByCategory } from "@//lib/FetchGame";
+import _ from 'lodash';
+
 
 export default async function Page({ params }) {
     const slug = params.slug;
   const games = await getGamesByCategory(slug);
 
-  return <ShowGames games={games} title={`${slug.toUpperCase()}`}/>;
+  return <ShowGames games={games} title={`${_.capitalize(slug)}`}/>;
 }

046f649a0ddb3a4f5746bcd7f74e85a334f78c80
basat <itzbasatmaqsood@gmail.com>
2025-02-04 15:07:04 +0500

    New games added

--- prisma/seed.js
+++ prisma/seed.js
@@ -47,7 +47,7 @@ async function main() {
       id: 1,
       title: "Aladdin",
       slug: "aladdin",
-      image: "cadillacs-and-dinosaurs.jpg",
+      image: "aladdin.jpg",
       description:
         "A vibrant action platformer based on Disney’s Aladdin, featuring acrobatic gameplay and sword fights as Aladdin tries to defeat Jafar and save Princess Jasmine.",
       game_url: "Aladdin.zip",
@@ -71,7 +71,7 @@ async function main() {
       id: 3,
       title: "Sonic and Knuckles",
       slug: "sonic-and-knuckles",
-      image: "disney-s-hercules.jpg",
+      image: "sonic3.jpg",
       description:
         "A fast-paced platformer featuring Sonic and Knuckles, each with unique abilities, battling Dr. Robotnik across various zones to stop his evil plans.",
       game_url: "sonic3.zip",
@@ -83,7 +83,7 @@ async function main() {
       id: 4,
       title: "The Amazing Spiderman",
       slug: "the-amazing-spiderman",
-      image: "biaofeng-zhanjing.jpg",
+      image: "spiderman.png",
       description:
         "A side-scrolling action game where Spider-Man battles villains like Mysterio and Venom while swinging across different levels.",
       game_url: "amazing-spiderman.zip",
@@ -95,7 +95,7 @@ async function main() {
       id: 5,
       title: "Batman Return of the Joker",
       slug: "batman-return-of-the-joker",
-      image: "street-fighter-ii.jpg",
+      image: "batman.jpg",
       description:
         "An action-packed game where Batman fights Joker’s minions through platforming and combat sequences.",
       game_url: "batman-return-of-joker.zip",
@@ -107,7 +107,7 @@ async function main() {
       id: 6,
       title: "Contra The Alien Wars",
       slug: "contra-the-alien-wars",
-      image: "captain-commando.jpg",
+      image: "contra.jpg",
       description:
         "A fast-paced run-and-gun shooter where players battle an alien invasion using powerful weapons and side-scrolling action.",
       game_url: "contra-alien-wars.zip",
@@ -119,7 +119,7 @@ async function main() {
       id: 7,
       title: "Super Mario 64",
       slug: "super-mario-64",
-      image: "super-mario-64.jpg",
+      image: "mario.png",
       description:
         "A groundbreaking 3D platformer where Mario explores Peach’s Castle, collecting Power Stars to rescue Princess Peach from Bowser.",
       game_url: "mario.zip",
@@ -131,7 +131,7 @@ async function main() {
       id: 8,
       title: "Streets of Rage 3",
       slug: "streets-of-rage-3",
-      image: "automobili-lamborghini.jpg",
+      image: "street3.png",
       description:
         "A classic beat ‘em up where players take on street gangs and a corrupt organization using hand-to-hand combat and weapons.",
       game_url: "streets-of-rage-3.zip",
@@ -143,7 +143,7 @@ async function main() {
       id: 9,
       title: "River Raid 1982",
       slug: "river-raid-1982",
-      image: "centre-court-tennis.jpg",
+      image: "riverraid.png",
       description:
         "A vertically scrolling shooter where players pilot a fighter jet over a river, destroying enemy bridges, ships, and helicopters while managing fuel.",
       game_url: "riverraid1982.zip",
@@ -155,7 +155,7 @@ async function main() {
       id: 10,
       title: "PitFall 1982",
       slug: "pitfall-1982",
-      image: "rayman-2-the-great-escape.jpg",
+      image: "pitfall.png",
       description:
         "One of the earliest platformers, where players guide Pitfall Harry through jungles filled with obstacles like quicksand, scorpions, and swinging vines to collect treasures.",
       game_url: "pitfall1982.zip",
@@ -167,7 +167,7 @@ async function main() {
       id: 11,
       title: "Teenage Mutant Ninja Turtles",
       slug: "ninja-turtle",
-      image: "rayman-2-the-great-escape.jpg",
+      image: "ninja.png",
       description:
         "A side-scrolling action game where players control the four Ninja Turtles, switching between them to battle enemies and rescue April O'Neil from Shredder’s forces.",
       game_url: "NinjaTurtles.zip",
@@ -179,7 +179,7 @@ async function main() {
       id: 12,
       title: "Ultimate Mortal Kombat 3",
       slug: "ultimate-mortal-kombat-3",
-      image: "teenage-mutant-hero-turtles.jpg",
+      image: "mk3.png",
       description:
         "A legendary fighting game with brutal fatalities, featuring a large roster of characters like Scorpion, Sub-Zero, and Liu Kang.",
       game_url: "mk3.zip",
@@ -191,7 +191,7 @@ async function main() {
       id: 13,
       title: "Super Mario Bros 3",
       slug: "super-mario-bros-3",
-      image: "teenage-mutant-hero-turtles.jpg",
+      image: "mario3.png",
       description:
         "With the familiar travel genre, join “Super Mario Bros. 3 ”will bring many interesting adventures. Similar to other versions of “Super Mario”, you will participate in controlling “Luigi or Mario” to overcome many different challenges of the game. When participating in the challenge, you will have to overcome 8 different kingdoms. ",
       game_url: "mario3.zip",
@@ -203,7 +203,7 @@ async function main() {
       id: 14,
       title: "Duck Tales",
       slug: "duck-tales",
-      image: "teenage-mutant-hero-turtles.jpg",
+      image: "ducktales.jpg",
       game_url: "ducktales.zip",
       description:
         "Scrooge McDuck is a wealthy boss who loves rare treasures. Coming to DuckTales, you will be on the journey to find 5 special treasures of Scrooge McDuck. In addition, these 5 treasures will be hidden in 5 different lands. To be able to collect all 5 treasures, you must overcome many obstacles and defeat the bosses at the end of each land.",
@@ -215,7 +215,7 @@ async function main() {
       id: 15,
       title: "BattleTanx",
       slug: "battle-tanx",
-      image: "teenage-mutant-hero-turtles.jpg",
+      image: "battletanx.png",
       game_url: "battle-tanx.zip",
       description:
         "BattleTanx (Nintendo 64) is a unique 3D tank combat game set in a post-apocalyptic world where players engage in fast-paced, arena-style battles. As a member of one of several factions, players control heavily armored tanks, navigating various environments like urban landscapes, military bases, and other war-torn settings.",
